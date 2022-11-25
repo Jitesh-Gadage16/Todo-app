5034090bd1f736db5f650ccfb03dff95b59251a7
Jitesh Gadage <gadagejitesh16@gmail.com>
2022-11-25 08:11:46 +0530

    done with the backend Curd Opeartion

---
+++ model/Todo.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const TodoSchema = new Schema({
+    title: String,
+    tasks: [String]
+})
+
+
+const TodoModel = mongoose.model("Todo",TodoSchema)
+
+
+export default TodoModel
